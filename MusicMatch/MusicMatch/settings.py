c364c3dfa7c0d040b726e7734e7d5e5fc4e73b01
Nick van Santen <vansantennick@gmail.com>
2020-09-07 19:52:42 +0200

    Test

--- MusicMatch/MusicMatch/settings.py
+++ MusicMatch/MusicMatch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = get_env_var("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', "rocky-chamber-86282.herokuapp.com/"]
+ALLOWED_HOSTS = ["rocky-chamber-86282.herokuapp.com/"]
 
 
 # Application definition
